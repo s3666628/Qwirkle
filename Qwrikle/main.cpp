21b58f7b721b77804a5434f5b12bd1875441bec7
Philip Beeby <pbeeby@mac.com>
2021-07-15 19:11:33 +1000

    Initial Commit

---
+++ Qwrikle/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Qwrikle
+//
+//  Created by Philip Beeby on 15/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
