3e42659c21613f12553f86b4d47982523986e30b
Philip Beeby <pbeeby@mac.com>
2021-07-18 14:30:48 +1000

    Adding Student Class, with setters and getters so we can create Student information for the Credits menu item

---
+++ Student.h
@@ -0,0 +1,43 @@
+//
+//  Student.h
+//  Qwrikle
+//
+//  Created by Philip Beeby on 18/7/21.
+//
+
+#ifndef Student_h
+#define Student_h
+
+#include <stdio.h>
+#include <string>
+class Student
+{
+public:
+    //defaul constructor
+    Student();
+    //constructor
+    Student(std::string name, std::string studentID,std::string email);
+    //destructor
+    ~Student();
+    //copy constructor
+    Student(Student& other);
+    // setters
+    void setName(std::string name);
+    void setstudentID(std::string studentID);
+    void setEmail(std::string email);
+ 
+    // getters
+    std::string getName();
+    std::string getstudentID();
+    std::string getEmail();
+    //other
+    void printStudentDetails();
+
+private:
+    // these have to be pointers so we can create class attributes on the heap
+    std::string* name;
+    std::string* studentID;
+    std::string* email;
+};
+
+#endif /* Student_h */
--- qwirkle.cpp
+++ qwirkle.cpp
@@ -2,6 +2,7 @@
 #include "LinkedList.h"
 
 #include <iostream>
+#include "Student.h"
 
 #define EXIT_SUCCESS    0
 
@@ -10,6 +11,24 @@ int main(void) {
    delete list;
 
    std::cout << "TODO: Implement Qwirkle soon!!" << std::endl;
+    //Student Testing - can move this test somewhere else when the time comes
+    // testing that we can create a new student
+    std::cout << "Creating a new Test Student 01" << std::endl;
+    Student* phil = new Student("Philip Beeby", "S3666628", "s3666628@student.rmit.edu.au");
+    std::cout << "Printing out Test Student 01 -  name: " << phil->getName() << std::endl;
+    std::cout << "Printing out Test Student 01 -  StudentID: " << phil->getstudentID()<< std::endl;
+    std::cout << "Printing out Test Student 01 -  email: " << phil->getEmail()<< std::endl;
+    std::cout << "Updating details of Test Student 01 to test Setter methods" << std::endl;
+    phil->setName("Paul");
+    phil->setstudentID("S366225");
+    phil->setEmail("paul.beeby@mac.com.au");
+    std::cout << "Printing out the student details" << std::endl;
+    phil->printStudentDetails();
+    std::cout << "Deleting Test Student 01 from Heap: " << std::endl;
+    
+    delete phil;
+    
+    
 
    return EXIT_SUCCESS;
 }
