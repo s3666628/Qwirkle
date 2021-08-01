c6bd4f8542954d72a61ddd1636792ff5b6d401ac
Philip Beeby <pbeeby@mac.com>
2021-08-02 08:25:43 +1000

    adding player.cpp player.h which has been created by Gordon

---
+++ Player.cpp
@@ -0,0 +1,63 @@
+// s3831786 Gordon Richard - RMIT
+
+#include "Player.h"
+
+using namespace std;
+
+Player::Player(std::string name)
+{
+    this->name = name;
+}
+
+Player::~Player()
+{
+}
+
+Player::Player(std::string name, LinkedList *hand)
+{
+    this->name = name;
+    this->hand = hand;
+}
+
+void Player::setPlayerHand(LinkedList *hand)
+{
+    this->hand = hand;
+}
+
+void Player::setPlayerScore(int score)
+{
+    this->score = score;
+}
+
+void Player::addPlayerScore(int score)
+{
+    this->score += score;
+}
+
+LinkedList *Player::getPlayerHand()
+{
+    return hand;
+}
+
+int *Player::getPlayerScorePtr()
+{
+    int *scorePtr = &score;
+    return scorePtr;
+}
+
+int Player::getPlayerScore()
+{
+
+    return score;
+}
+
+string Player::getPlayerName()
+{
+
+    return name;
+}
+string *Player::getPlayerNamePtr()
+{
+    string *namePtr = &name;
+    return namePtr;
+}
