6a7d2b08f0c6f45f6a85a8a7979673169ac34600
GBRic <58135792+GBRic@users.noreply.github.com>
2021-08-04 05:45:10 +1000

    Board and player added
    made additions to Tile.cpp and have added board and player 
    
    have also added two lines to qwirkle.cpp in order to test printing the board

--- Player.h
+++ Player.h
@@ -4,6 +4,8 @@
 
 #include <string>
 
+#include <ostream>
+
 class Player
 {
 
--- Tile.cpp
+++ Tile.cpp
@@ -3,24 +3,37 @@
 #include <iostream>
 
 //Constructor to create a new shape
-Tile::Tile(Colour colour, Shape shape){
+Tile::Tile(Colour colour, Shape shape)
+{
     // default contructor
     std::cout << "Creating a new Shape Object: " << std::endl;
     this->colour = new Colour(colour);
     this->shape = new Shape(shape);
-
 }
-Tile::~Tile(){
+Tile::~Tile()
+{
     // destructor
     std::cout << "Deleting Shape Object and Attributes: " << std::endl;
     delete colour;
     delete shape;
 }
-Colour Tile::getTileColour(){
+Colour Tile::getTileColour()
+{
     return *colour;
-    
 }
-Shape Tile::getTileShape(){
+Shape Tile::getTileShape()
+{
     return *shape;
 }
 
+void Tile::printColoured()
+{
+    if (outputStream != &std::cout)
+    {
+        printTile();
+    }
+    else
+    {
+        // Colour codes a la this-> color == a color
+    }
+}
